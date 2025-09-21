7182c4dfa1566881a1f424a3dc80a80f7d0eb403
Aditya Shukla <Shadowshukla76@gmail.com>
2025-09-21 15:42:44 +0530

    remove env file

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,1 +1,15 @@
 package db
+
+import (
+	"fmt"
+	"goCal/internal/logger"
+	"os"
+)
+
+func DBConnect() {
+	DATABASE_URL := os.Getenv("DATABASE_URL")
+	if DATABASE_URL == "" {
+		logger.Error(`Failed to get the database url`)
+		fmt.Printf(`Failed to get the database url`)
+	}
+}
