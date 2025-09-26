4172cc2aee2df249f546c56842e8e2a87a841047
Aditya Shukla <Shadowshukla76@gmail.com>
2025-09-26 15:20:24 +0530

    Added Create User schema

--- internal/routes/user.router.go
+++ internal/routes/user.router.go
@@ -1,11 +1,69 @@
 package routes
 
 import (
+	"fmt"
+	"goCal/internal/db"
+	"goCal/internal/schema"
+	"net/http"
+	"strings"
+
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 )
 
+func hashedPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(bytes), err
+}
+
 func UserRoutes(router *gin.RouterGroup) {
-	router.GET("/", func(ctx *gin.Context) {
+	router.POST("/", func(ctx *gin.Context) {
+		var user *schema.User
+		if err := ctx.ShouldBindJSON(&user); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error(), "success": false})
+			return
+		}
+
+		custom_link := strings.Split(user.Username, " ")[0] + "-" + strings.Split(user.Email, "@")[0]
+
+		query := `
+		INSERT INTO users (username, email, password, profileUrl, country, pronouns,custom_link)
+		VALUES ($1,$2,$3,$4,$5,$6,$7)
+		RETURNING id,custom_link,created_at,welcome_message,isVerified,date_format,time_format,timezone;
+		`
+
+		hashedPassword, err := hashedPassword(user.Password)
+		if err != nil {
+			fmt.Println("Error hashing password:", err)
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Error hashing password", "success": false})
+			return
+		}
+		row := db.Conn.QueryRow(ctx, query, user.Username, user.Email, hashedPassword, user.ProfileUrl, user.Country, user.Pronouns, custom_link)
+
+		if err := row.Scan(&user.ID, &user.CustomLink, &user.CreatedAt, &user.WelcomeMessage, &user.IsVerified, &user.DateFormat, &user.TimeFormat, &user.Timezone); err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error(), "success": false})
+			return
+		}
+
+		user.Password = ""
+
+		ctx.JSON(http.StatusCreated, gin.H{
+			"user": gin.H{
+				"id":              user.ID,
+				"username":        user.Username,
+				"email":           user.Email,
+				"country":         user.Country,
+				"welcome_message": user.WelcomeMessage,
+				"timezone":        user.Timezone,
+				"pronouns":        user.Pronouns,
+				"isverified":      user.IsVerified,
+				"date_format":     user.DateFormat,
+				"time_format":     user.TimeFormat,
+				"custom_link":     user.CustomLink,
+				"created_at":      user.CreatedAt,
+			},
+			"success": true,
+		})
 
 	})
 }
--- internal/schema/user.schema.go
+++ internal/schema/user.schema.go
@@ -1,4 +1,20 @@
 package schema
 
+import "time"
+
 type User struct {
+	ID             string    `json:"id" db:"id"`
+	Username       string    `json:"username" binding:"required"`
+	Email          string    `json:"email" binding:"required"`
+	Password       string    `json:"password" binding:"required"`
+	ProfileUrl     string    `json:"profileUrl,omitempty"`
+	Country        string    `json:"country" binding:"required"`
+	WelcomeMessage string    `json:"welcome_message,omitempty"`
+	Timezone       string    `json:"timezone,omitempty"`
+	Pronouns       string    `json:"pronouns,omitempty"`
+	IsVerified     bool      `json:"isverified" db:"isverified"`
+	DateFormat     string    `json:"date_format,omitempty"`
+	TimeFormat     string    `json:"time_format,omitempty"`
+	CustomLink     string    `json:"custom_link,omitempty"`
+	CreatedAt      time.Time `json:"created_at,omitempty"`
 }
