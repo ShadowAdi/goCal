3d47ccb0659bf4104b78faf7e9c0d6f5d8e7a10a
Aditya Shukla <Shadowshukla76@gmail.com>
2025-10-24 12:18:40 +0530

    added fol;der schema

---
+++ internal/schema/folder.schema.go
@@ -0,0 +1,12 @@
+package schema
+
+import "github.com/google/uuid"
+
+type Folder struct {
+	ID                uuid.UUID `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()" json:"id"`
+	FolderName        string    `gorm:"uniqueIndex;not null;size:200" json:"folder_name" validate:"required,min=3,max=50"`
+	FolderDescription string    `gorm:"not null;size:500" json:"folder_description"`
+	FolderTags        []string  `gorm:"type:text[];default:[]" json:"folder_tags"`
+	CreatedById       uuid.UUID `gorm:"type:uuid;not null" json:"created_by"`
+	createdBy         User      `gorm:"constraint:OnUpdate:CASCADE,OnDelete:OnDelete;" json:"-"`
+}
