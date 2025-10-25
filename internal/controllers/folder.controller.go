015443c96c2f96d81004b67b20ab31b0ac664008
Aditya Shukla <Shadowshukla76@gmail.com>
2025-10-25 18:59:12 +0530

    added controller to get updated folder

--- internal/controllers/folder.controller.go
+++ internal/controllers/folder.controller.go
@@ -201,3 +201,77 @@ func (fo *FolderController) DeleteFolder(ctx *gin.Context) {
 	return
 
 }
+
+func (fo *FolderController) UpdateFolder(ctx *gin.Context) {
+	id := ctx.Param("id")
+	if id == "" {
+		logger.Error("Failed to get the id in the request ", id)
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"success": false,
+			"message": "Failed to get the id of the request",
+		})
+	}
+
+	userId, exists := ctx.Get("userId")
+	if !exists {
+		logger.Error("Error getting userId")
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"success": false,
+			"error":   "Not Authorized",
+		})
+	}
+
+	userIdStr, ok := userId.(string)
+	if !ok || userIdStr == "" {
+		logger.Error("Error parsing userId\n")
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"success": false,
+			"error":   "Invalid User Id type in context",
+		})
+		return
+	}
+
+	loggedInUserFound, loggedInUserError := fo.UserService.GetUser(userIdStr)
+	if loggedInUserError != nil {
+		logger.Error("Error finding logged-in user: %v\n", loggedInUserError)
+		ctx.JSON(http.StatusNotFound, gin.H{
+			"success": false,
+			"error":   loggedInUserError.Error(),
+		})
+		return
+	}
+
+	if loggedInUserFound.IsVerified == false {
+		ctx.JSON(http.StatusForbidden, gin.H{
+			"success": false,
+			"error":   "User Is Not Verified",
+		})
+	}
+
+	var updateRequest *schema.UpdateFolderRequest
+	if err := ctx.ShouldBindJSON(&updateRequest); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"success": false,
+			"error":   err.Error(),
+		})
+		return
+	}
+
+	updatedFolder, error := fo.FolderService.UpdateFolder(updateRequest, id, userIdStr)
+
+	if error != nil {
+		logger.Error("Error updating folder %v\n", error.Error())
+		ctx.JSON(http.StatusNotFound, gin.H{
+			"success": false,
+			"error":   error.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"success": true,
+		"folder":  updatedFolder,
+	})
+	return
+
+}
