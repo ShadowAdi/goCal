146de5b42f8ed26cc743ef8f0707fb75e05cb577
Aditya Shukla <Shadowshukla76@gmail.com>
2025-10-27 11:17:29 +0530

    added service to upload file

--- internal/services/FileStorage.service.go
+++ internal/services/FileStorage.service.go
@@ -1,26 +1,35 @@
 package services
 
 import (
+	"bytes"
 	"errors"
+	"fmt"
 	"goCal/internal/logger"
 	"io"
+	"path/filepath"
+	"time"
+
+	storage_go "github.com/supabase-community/storage-go"
 )
 
 type FileStorageService struct {
+	storageClient *storage_go.Client
 }
 
-func NewFileStorageService() *FileStorageService {
-	return &FileStorageService{}
+func NewFileStorageService(client *storage_go.Client) *FileStorageService {
+	return &FileStorageService{
+		storageClient: client,
+	}
 }
 
-func (nfs *FileStorageService) UploadFile(userId string, fileName string, file io.Reader, fileType string) error {
+func (nfs *FileStorageService) UploadFile(userId string, fileName string, file io.Reader, fileType string) (string, error) {
 	if userId == "" {
 		logger.Error("Failed to get the userId UnAuthorized")
-		return errors.New("Unauthorized User. UserId Not Found")
+		return "", errors.New("Unauthorized User. UserId Not Found")
 	}
 	if fileName == "" {
 		logger.Error("Failed to get the File Name")
-		return errors.New("Failed to get the fileName")
+		return "", errors.New("Failed to get the fileName")
 	}
 
 	var bucketName string
@@ -37,8 +46,26 @@ func (nfs *FileStorageService) UploadFile(userId string, fileName string, file i
 		bucketName = "goCal-Other-Bucket"
 	}
 
-	// TODO: Implement actual file upload logic using bucketName
-	logger.Info("File will be uploaded to bucket: " + bucketName)
+	timeStamp := time.Now().Unix()
+	fileExt := filepath.Ext(fileName)
+	baseFileName := fileName[:len(fileName)-len(fileExt)]
+	uniqueFileName := fmt.Sprintf("%s%d_%s%s", userId, timeStamp, baseFileName, fileExt)
+	logger.Info(fmt.Sprintf("Uploading file %s to bucket: %s", uniqueFileName, bucketName))
 
-	return nil
+	fileBytes, err := io.ReadAll(file)
+	if err != nil {
+		logger.Error(fmt.Sprintf("Failed to read file: %v", err))
+		return "", errors.New("failed to read file: %w " + err.Error())
+	}
+
+	_, errUpload := nfs.storageClient.UploadFile(bucketName, uniqueFileName, bytes.NewReader(fileBytes))
+	if errUpload != nil {
+		logger.Error(fmt.Sprintf("Failed to upload file to supabase %v ", err))
+		return fmt.Sprintf("Failed to upload file to supabase %v ", err), nil
+	}
+
+	publicURL := nfs.storageClient.GetPublicUrl(bucketName, uniqueFileName)
+	logger.Info(fmt.Sprintf("File uploaded successfully. URL: %s", publicURL.SignedURL))
+
+	return publicURL.SignedURL, nil
 }
