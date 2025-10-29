93154495c81615f09e44ed91ddc6f546aa43b3dd
Aditya Shukla <Shadowshukla76@gmail.com>
2025-10-29 07:53:42 +0530

    added update field service

--- internal/services/Folder.service.go
+++ internal/services/Folder.service.go
@@ -58,11 +58,13 @@ func (fo *FolderService) CreateFolder(userId string, folderData *schema.Folder) 
 	errFoundFolder := db.DB.Where("folder_name = ? AND created_by = ?", folderData.FolderName, userId).First(&existingFolder).Error
 
 	if errFoundFolder != nil {
+		logger.Error(`Failed to get folder %w`, errFoundFolder.Error())
 		return nil, errFoundFolder
 	}
 
 	result := db.DB.Create(&existingFolder)
 	if result.Error != nil {
+		logger.Error(`Failed to create folder %w`, result.Error)
 		return nil, result.Error
 	}
 	return fo.GetFolderByNameForUser(existingFolder.FolderName, userId)
@@ -73,12 +75,47 @@ func (fo *FolderService) DeleteFolder(userId string, folderId string) (string, e
 	errFoundFolder := db.DB.Where("id = ? AND created_by = ?", folderId, userId).First(&existingFolder).Error
 
 	if errFoundFolder != nil {
+		logger.Error(`Failed to delete folder. Folder Not Found %w`, errFoundFolder.Error())
 		return "Failed to Delete Folder. Folder Not Found", errFoundFolder
 	}
 
 	result := db.DB.Delete(&existingFolder)
 	if result.Error != nil {
+		logger.Error(`Failed to delete folder. %w`, result.Error)
 		return "Failed to delete Folder.", result.Error
 	}
 	return "Folder Deleted Successfully", nil
+}
+
+func (fo *FolderService) UpdateFolder(userId string, folderId string, folderData *schema.UpdateFolderRequest) (*schema.Folder, error) {
+	var existingFolder *schema.Folder
+	errFoundFolder := db.DB.Where("id = ? AND created_by = ?", folderId, userId).First(&existingFolder).Error
+
+	if errFoundFolder != nil {
+		logger.Error(`Failed to Update folder. Not Found %w`, errFoundFolder.Error())
+		return nil, errFoundFolder
+	}
+
+	updatedFields := make(map[string]interface{})
+
+	if folderData.FolderName != nil {
+		updatedFields["folder_name"] = *folderData.FolderName
+	}
+
+	if folderData.FolderDescription != nil {
+		updatedFields["folder_description"] = *folderData.FolderDescription
+	}
+
+	if folderData.FolderTags != nil {
+		updatedFields["folder_tags"] = folderData.FolderTags
+	}
+
+	if len(updatedFields) > 0 {
+		if err := db.DB.Model(&schema.Folder{}).Where("id = ? AND created_by = ?", folderId, userId).Updates(updatedFields); err != nil {
+			logger.Error("Failed to Update Folder %w", err)
+			return nil, err.Error
+		}
+	}
+
+	return existingFolder, nil
 }
