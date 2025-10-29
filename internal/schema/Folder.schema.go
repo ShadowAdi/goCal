bd6037f051995109e6793715f97b03b272e7f008
Aditya Shukla <Shadowshukla76@gmail.com>
2025-10-29 08:24:53 +0530

    Fix Array Type checks

--- internal/schema/Folder.schema.go
+++ internal/schema/Folder.schema.go
@@ -2,16 +2,17 @@ package schema
 
 import (
 	"github.com/google/uuid"
+	"github.com/lib/pq"
 	"gorm.io/gorm"
 )
 
 type Folder struct {
-	ID                uuid.UUID `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()" json:"id"`
-	FolderName        string    `gorm:"uniqueIndex;not null;size:200" json:"folder_name" validate:"required,min=3,max=50"`
-	FolderDescription string    `gorm:"size:500" json:"folder_description"`
-	FolderTags        []string  `gorm:"type:text[]" json:"folder_tags"`
-	CreatedById       uuid.UUID `gorm:"column:created_by;type:uuid;not null" json:"created_by"`
-	createdBy         User      `gorm:"constraint:OnDelete:CASCADE;" json:"-"`
+	ID                uuid.UUID      `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()" json:"id"`
+	FolderName        string         `gorm:"uniqueIndex;not null;size:200" json:"folder_name" validate:"required,min=3,max=50"`
+	FolderDescription string         `gorm:"size:500" json:"folder_description"`
+	FolderTags        pq.StringArray `gorm:"type:text[]" json:"folder_tags"`
+	CreatedById       uuid.UUID      `gorm:"column:created_by;type:uuid;not null" json:"created_by"`
+	createdBy         User           `gorm:"constraint:OnDelete:CASCADE;" json:"-"`
 }
 
 type UpdateFolderRequest struct {
