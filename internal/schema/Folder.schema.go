85bebe549a65009a3294f7868c4a0f877290add4
Aditya Shukla <Shadowshukla76@gmail.com>
2025-10-29 06:18:25 +0530

    Fix the schema

--- internal/schema/Folder.schema.go
+++ internal/schema/Folder.schema.go
@@ -1,25 +1,30 @@
 package schema
 
-type Folder Struct{
-	ID uuid.UUID `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()" json:"id"`
-	FolderName string `gorm:"uniqueIndex;not null;size:200" json:"folder_name" validate:"required,min=3,max=50"`
-	FolderDescription string `gorm:"size:500" json:"folder_description"`
-	FolderTags []string `gorm:"type:text[]" json:"folder_tags"`
-	CreatedById uuid.UUID `gorm:"type:uuid;not null" json:"created_by"`
-	createdBy User `gorm:"constraint:OnDelete:CASCADE;" json:"-"`
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type Folder struct {
+	ID                uuid.UUID `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()" json:"id"`
+	FolderName        string    `gorm:"uniqueIndex;not null;size:200" json:"folder_name" validate:"required,min=3,max=50"`
+	FolderDescription string    `gorm:"size:500" json:"folder_description"`
+	FolderTags        []string  `gorm:"type:text[]" json:"folder_tags"`
+	CreatedById       uuid.UUID `gorm:"type:uuid;not null" json:"created_by"`
+	createdBy         User      `gorm:"constraint:OnDelete:CASCADE;" json:"-"`
 }
 
-type UpdateFolderRequest struct{
+type UpdateFolderRequest struct {
 	FolderName        *string   `json:"folder_name,omitempty" validate:"omitempty,min=3,max=50"`
-	FolderDescription *string `json:"folder_description"`
+	FolderDescription *string   `json:"folder_description"`
 	FolderTags        []*string `json:"folder_tags"`
 }
 
-func (Folder) TableName() string{
+func (Folder) TableName() string {
 	return "folders"
 }
 
-func (fo *Folder) BeforeCreate(tx *gorm.DB) (err error)  {
-	fo.ID=uuid.New()
+func (fo *Folder) BeforeCreate(tx *gorm.DB) (err error) {
+	fo.ID = uuid.New()
 	return nil
 }
